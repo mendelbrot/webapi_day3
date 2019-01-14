e39a4c831b49b135596fc0b45327cd3e18ffe67d
Gregory Maxedon <gregory.maxedon@gmail.com>
2019-01-14 09:33:22 -0800

    first commit

--- webapi_day3/Startup.cs
+++ webapi_day3/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using webapi_day3.DatabaseHelper;
 
 namespace webapi_day3
 {
@@ -24,6 +26,9 @@ namespace webapi_day3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(options =>
+            options.UseSqlite("Data Source=.\\wwwroot\\sql.db"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
